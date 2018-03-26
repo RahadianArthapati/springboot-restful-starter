2c4fd42287d8be13270218f83d11d747dd56eeeb
RahadianArthapati <rahadian.artha@gmail.com>
2018-03-26 08:36:35 +0700

    init project

---
+++ src/main/java/com/grandline/springbootstarter/SpringbootStarterApplication.java
@@ -0,0 +1,12 @@
+package com.grandline.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStarterApplication.class, args);
+	}
+}
